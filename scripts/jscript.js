f5a5ba378c8477df257311724427ad3f9404365a
Darz_K <darrenkellydk@rogers.com>
2018-03-07 23:52:23 -0500

    updated player controls..play works when no track selected/display track info

--- scripts/jscript.js
+++ scripts/jscript.js
@@ -23,41 +23,71 @@ let audio = new Audio();
 $('#trackNames li h2').on('click', playTune);
 
 function playTune(){ //when title clicked in songlist
-    if (!audio.currentTime) {
-        $('#durationTime').html('0.00');
-        audio.src = 'media/' + songs[0];
-        let songIndexNumber = songs[0];
-        audio.play();
-    
-    }
 
     $('#trackNames li').removeClass('active'); //remove active class from unactive li's
-        songIndexNumber = $(this).parents("li").index();
+        let songIndexNumber = $(this).parents("li").index();
         let listClass = $(this).parents("li").addClass('active'); //add active class
         let songCurrentTitle = $(this).text(); //get current song title
         $('#trackTitle h1').text('Track Title : ' + songCurrentTitle); //display current song title on track title area
+        //add a '0' if track number is less than 10
+        if (songIndexNumber < 9) {
+        $('#trackNumber h1').text('Track No: 0' + (songIndexNumber + 1)); //display current track number on track number area
+        }
+        else{
         $('#trackNumber h1').text('Track No: ' + (songIndexNumber + 1)); //display current track number on track number area
-        
+        }
+
+        if (!audio.currentTime) {
+            $('#durationTime').html('0.00');
+            audio.src = 'media/' + songs[0];
+        audio.play();
+        }
         audio.src = 'media/' + songs[songIndexNumber];
         audio.play();
         $('#playButton').hide();
         $('#pauseButton').show();
+        displayDuration();
+        
 };
+
 //play button
 $('#playButton').click(function(){
+    //if no track selected yet, play the first one
     if (!audio.currentTime) {
-        // audio.src = 'media/' + songs[currentSong];
-        // audio.play();
-        playTune();
-        // console.log('reading');
+        $('#trackTitle h1').text('Track Title : R30 Overture'); //display current song title on track title area
+        $('#trackNumber h1').text('Track No: 01'); //display current track number on track number area
+        audio.src = 'media/' + songs[currentSong];
+        audio.play();
+        // playTune();
+    displayDuration();
+        
     }
     audio.play();
     $('#pauseButton').show();
     $('#playButton').hide();
+    displayDuration();
 });
+
 //pause button
 $('#pauseButton').click(function(){
     audio.pause();
     $('#pauseButton').hide();
     $('#playButton').show();
 });
+
+//time duration
+function displayDuration(){
+    $(audio).on('timeupdate' , function(){
+        let secs = parseInt(audio.currentTime % 60);
+        let mins = parseInt((audio.currentTime)/ 60)  % 60;
+        if (secs < 10) {
+            secs = '0' + secs;
+        }
+        $('#durationTime').text(mins + '.' + secs);
+        let currentTimeValue = 0;
+        if (audio.currentTime > 0) {
+            currentTimeValue = ((100 / audio.duration) * audio.currentTime);
+        }
+        $('#displayProgress').css('width', currentTimeValue + '%')
+    });
+}
